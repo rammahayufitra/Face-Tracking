3f460e2f3626383f07b41caa866b709b650a4470
ramma.fitra <rammahayufitra@gmail.com>
2023-03-02 02:25:20 +0700

    [DEV] tracking berhasil butuh refactoring

--- components/inference.py
+++ components/inference.py
@@ -19,7 +19,10 @@ def streaming():
             data = ct.Update(results)
             for object in results:
                 box = BBOX(object)
-            Draw(frame, box)
+            for val in data:
+                cv2.circle(frame,(box.xywh[0]+int(box.xywh[2]/2),box.xywh[1]+int(box.xywh[3]/2)),5,[0,0,255],-1)
+                cv2.putText(frame, str(val[3]),(box.xywh[0]+int(box.xywh[2]/2),box.xywh[1]+int(box.xywh[3]/2)), cv2.FONT_HERSHEY_SIMPLEX , 1, [0,0,255], 4, cv2.LINE_AA) 
+                Draw(frame, box)
             cv2.imshow("webcam", frame)
             if cv2.waitKey(1) == 27:
                 break
